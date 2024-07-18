a9042b2cc2fb1a3f5327711cdc016c6e93be62b9
Carine <carinebarros@outlook.com>
2024-07-18 08:56:44 -0300

    feat: Configs NativeWind

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Profile } from "@/app/profile";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import "@/styles/global.css";
+
+const App = () => (
+  <>
+    <Profile />
+    <StatusBar style="auto" />
+  </>
+);
+
+export default App;
---
+++ src/app/profile.tsx
@@ -0,0 +1,7 @@
+import { Text, View } from "react-native";
+
+export const Profile = () => (
+  <View className="flex-1 justify-center items-center">
+    <Text>Profile</Text>
+  </View>
+);
