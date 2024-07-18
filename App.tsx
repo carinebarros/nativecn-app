7c9ffca191f74c2dec5f85542676db63d5e091b4
Carine <carinebarros@outlook.com>
2024-07-18 11:13:33 -0300

    feat: Implement Toast feedback

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import "@/styles/global.css";
 import { StatusBar } from "expo-status-bar";
 
 import { Profile } from "@/app/profile";
+import { ToastProvider } from "@/components/Toast";
 
 const App = () => (
-  <>
+  <ToastProvider position="top">
     <Profile />
     <StatusBar style="light" />
-  </>
+  </ToastProvider>
 );
 
 export default App;
--- src/app/profile.tsx
+++ src/app/profile.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { Image, ScrollView, View } from "react-native";
 
 import { User } from "@/components/User";
@@ -5,25 +6,32 @@ import { Skills } from "@/components/Skills";
 import { Preferences } from "@/components/Preferences";
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
+import { useToast } from "@/components/Toast";
 
-export const Profile = () => (
-  <View className="flex-1 bg-gray-700">
-    <ScrollView>
-      <Image
-        source={require("@/assets/banner.png")}
-        className="w-full h-52 -mb-16"
-      />
+export const Profile = () => {
+  const { toast } = useToast();
 
-      <View className="flex-1 px-4 pb-4">
-        <User />
-        <Skills />
-        <Preferences />
+  const handleSave = useCallback(() => toast("Saved!", "success"), []);
 
-        <View className="w-full mt-6 flex-1">
-          <Input placeholder="Company" label="Company" inputClasses="mb-6" />
-          <Button label="Save" />
+  return (
+    <View className="flex-1 bg-gray-700">
+      <ScrollView>
+        <Image
+          source={require("@/assets/banner.png")}
+          className="w-full h-52 -mb-16"
+        />
+
+        <View className="flex-1 px-4 pb-4">
+          <User />
+          <Skills />
+          <Preferences />
+
+          <View className="w-full mt-6 flex-1">
+            <Input placeholder="Company" label="Company" inputClasses="mb-6" />
+            <Button label="Save" onPress={handleSave} />
+          </View>
         </View>
-      </View>
-    </ScrollView>
-  </View>
-);
+      </ScrollView>
+    </View>
+  );
+};
